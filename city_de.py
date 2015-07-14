3ece858077d536dac873f69e7df3eb2e9f783a8f
Max Shmidov <mshmidov@exadel.com>
2015-07-14 17:02:33 +0300

    tuning city_de

--- city_de.py
+++ city_de.py
@@ -3,7 +3,8 @@ __author__ = 'mshmidov'
 
 from table.markov_table import MarkovTable
 
-CITY_DE = MarkovTable(["seed/city_de.txt"], key_size=3, prettify_result=lambda line: line.title())
+CITY_DE = MarkovTable(["seed/city_de.txt"],
+                      key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True)
 
 if __name__ == '__main__':
     print(CITY_DE.roll())
