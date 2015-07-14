aee0afbe00fc8b8dca9e1d13e88602195347400c
Max Shmidov <mshmidov@exadel.com>
2015-07-14 17:31:34 +0300

    added surnames to german generator

---
+++ name_de.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import random
+import sys
+
+__author__ = 'mshmidov'
+
+from table.markov_table import MarkovTable
+
+NAME_M_DE = MarkovTable(["seed/name_m_de.txt"],
+                        key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True)
+
+NAME_F_DE = MarkovTable(["seed/name_f_de.txt"],
+                        key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True)
+
+SURNAME_DE = MarkovTable(["seed/surname_de.txt"],
+                         key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True)
+
+
+def german_name(male=True):
+    name_chain = NAME_M_DE if male else NAME_F_DE
+
+    name = []
+
+    for i in range(random.randint(1, 3)):
+        name.append(name_chain.roll())
+
+    name.append(SURNAME_DE.roll())
+
+    return " ".join(name)
+
+
+if __name__ == '__main__':
+    female = len(sys.argv) > 1 and sys.argv[1] == '-f'
+
+    print(german_name(not female))
--- name_f_de.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-__author__ = 'mshmidov'
-
-from table.markov_table import MarkovTable
-
-NAME_F_DE = MarkovTable(["seed/name_f_de.txt"], key_size=3, prettify_result=lambda line: line.title())
-
-if __name__ == '__main__':
-    print(NAME_F_DE.roll())
