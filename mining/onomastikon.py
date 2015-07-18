28b1bb1b7b2fd8c51bb06236953097739d5fab72
Max Shmidov <max.shmidov@gmail.com>
2015-07-18 23:49:08 +0300

    added name sorting and english surnames

--- mining/onomastikon.py
+++ mining/onomastikon.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import re
+import itertools
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,23 +10,42 @@ def load_names(page, filename):
     response = requests.get(page)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    male_header = next(header for header in soup.find_all('h3') if str(header.string).startswith('Female'))
     female_header = next(header for header in soup.find_all('h3') if str(header.string).startswith('Female'))
 
-    male_names = (name.string.strip() for name in female_header.find_all_previous('td') if name.string is not None)
-    female_names = (name.string.strip() for name in female_header.find_all_next('td') if name.string is not None)
+    male_names = (name.title() for name in
+                  (itertools.chain.from_iterable(
+                      re.findall(r"[\w]+", name.string) for name in reversed(female_header.find_all_previous('td'))
+                      if is_not_empty(name.string)))
+                  if not name.endswith("us"))
+
+    female_names = (name.title() for name in
+                    (itertools.chain.from_iterable(
+                        re.findall(r"[\w]+", name.string) for name in female_header.find_all_next('td')
+                        if is_not_empty(name.string)))
+                    if not name.endswith("us"))
 
     output_m = open(filename.format('m'), 'w')
-    for name in male_names:
-        if not name.isspace() and not name == '' and not name.endswith("us"):
-            for part in re.findall(r"[\w]+", name):
-                    output_m.write(part + '\n')
+    for name in sorted(list(set(male_names))):
+        output_m.write(name + '\n')
 
     output_f = open(filename.format('f'), 'w')
-    for name in female_names:
-        if not name.isspace() and not name == '' and not name.endswith("us"):
-            for part in re.findall(r"[\w]+", name):
-                output_f.write(part + '\n')
+    for name in sorted(list(set(female_names))):
+        output_f.write(name + '\n')
+
+
+def load_surnames(page, filename):
+    response = requests.get(page)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    surnames = (name.string.strip().split()[0].title() for name in soup.find_all('td') if is_not_empty(name.string))
+
+    output = open(filename.format('surnames'), 'w')
+    for name in sorted(list(set(surnames))):
+        output.write(name + '\n')
+
+
+def is_not_empty(s):
+    return s is not None and not s.isspace() and not s == ''
 
 
 load_names('http://tekeli.li/onomastikon/England-Medieval/Norman.html',
@@ -44,3 +65,12 @@ load_names('http://tekeli.li/onomastikon/England-Medieval/Saxon.html',
 
 load_names('http://tekeli.li/onomastikon/England-Medieval/Rarities.html',
            '../seed/england/medieval-rarities-{}.txt')
+
+load_surnames('http://tekeli.li/onomastikon/England-Surnames/Old-English.html',
+              '../seed/england/old-english-{}.txt')
+
+load_surnames('http://tekeli.li/onomastikon/England-Surnames/Tradenames.html',
+              '../seed/england/trade-{}.txt')
+
+load_surnames('http://tekeli.li/onomastikon/England-Surnames/Byname.html',
+              '../seed/england/byname-{}.txt')
--- name_en.py
+++ name_en.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import random
 import sys
 
 __author__ = 'mshmidov'
@@ -9,7 +8,6 @@ from table.markov_table import MarkovTable
 NAME_M_EN = MarkovTable(['seed/england/medieval-norman-m.txt',
                          'seed/england/medieval-norse-m.txt',
                          'seed/england/medieval-rarities-m.txt'],
-
                         key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True, limit=10)
 
 NAME_F_EN = MarkovTable(['seed/england/medieval-norman-f.txt',
@@ -17,19 +15,16 @@ NAME_F_EN = MarkovTable(['seed/england/medieval-norman-f.txt',
                          'seed/england/medieval-rarities-f.txt'],
                         key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True, limit=10)
 
-# SURNAME_EN = MarkovTable(["seed/surname_en.txt"],
-#                          key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True)
+SURNAME_EN = MarkovTable(['seed/england/old-english-surnames.txt',
+                          'seed/england/trade-surnames.txt',
+                          'seed/england/byname-surnames.txt'],
+                         key_size=3, prettify_result=lambda line: line.title(), exclude_exact_matches=True)
 
 
 def english_name(male=True):
     name_chain = NAME_M_EN if male else NAME_F_EN
 
-    name = []
-
-    for i in range(random.randint(1, 3)):
-        name.append(name_chain.roll())
-
-    # name.append(SURNAME_EN.roll())
+    name = [name_chain.roll(), SURNAME_EN.roll()]
 
     return " ".join(name)
 
